3ef01226cc12801e1f3c2919562189dc31b088ba
Brandon Stone <brandonrstone@gmail.com>
2022-06-07 18:37:53 -0400

    Search function implemented, card styling added

--- src/App.js
+++ src/App.js
@@ -1,78 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import './App.css'
 
-/* 
+import PokemonList from './PokemonList'
 
-1.) Why do you need to use the functin version of useState() ? 
-
-*/
-
-export default function PokemonList() {
-  const [pokemonList, setPokemonList] = useState([])
-  const [search, setSearch] = useState('')
-
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=500`)
-      .then(res => res.json())
-      .then(allPokemon =>
-        allPokemon.results.forEach(pokemon => {
-          fetch(pokemon.url)
-            .then(res => res.json())
-            .then(data => setPokemonList(current => [...current, data]))
-            .catch(err => `Error: ${err}`)
-        })
-      )
-      .catch(err => `Error: ${err}`)
-  }, [])
-
-  const Pokemon = props => (
-    <div className='pokemon-div'>
-      <h3>{`${props.name[0].toUpperCase() + props.name.slice(1)}`}</h3>
-      <img src={props.sprites.front_default} alt={'Pokemon Sprite'} />
-      <p>
-        {`Abilities: ${
-          props.abilities[0].ability.name[0].toUpperCase() +
-          props.abilities[0].ability.name.slice(1)
-        }`}
-      </p>
-    </div>
-  )
-
-  const filteredPokemon = pokemonList.filter(pokemon =>
-    pokemon.name.toLowerCase().includes(search.toLowerCase())
-  )
-
-  const regi = []
-  regi.push(pokemonList[375])
-  console.log(regi)
-  console.log(pokemonList)
-
+export default function App() {
   return (
-    <div>
-      <input value={search} onChange={e => setSearch(e.target.value)} />
-      {pokemonList.map(pokemon => {
-        return <Pokemon key={pokemon.id} {...pokemon} />
-      })}
-      {/* {search !== ''
-        ? filteredPokemon.map(pokemon => (
-            <div key={pokemon.id} className='pokemon-div'>
-              <PokemonCard
-                value={`#${pokemon.id} ${
-                  pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-                }`}
-              />
-              <img src={pokemon.sprites.front_default} alt={'Pokemon Sprite'} />
-            </div>
-          ))
-        : pokemonList.map(pokemon => (
-            <div key={pokemon.id} className='pokemon-div'>
-              <PokemonCard
-                value={`#${pokemon.id} ${
-                  pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-                }`}
-              />
-              <img src={pokemon.sprites.front_default} alt={'Pokemon Sprite'} />
-            </div>
-          ))} */}
-    </div>
+    <>
+      <PokemonList />
+    </>
   )
 }
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,5 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function PokemonList() {
-  return <div>PokemonList</div>
+  const [pokemonList, setPokemonList] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=500`)
+      .then(res => res.json())
+      .then(allPokemon =>
+        allPokemon.results.forEach(pokemon => {
+          fetch(pokemon.url)
+            .then(res => res.json())
+            .then(data => setPokemonList(current => [...current, data]))
+            .catch(err => `Error: ${err}`)
+        })
+      )
+      .catch(err => `Error: ${err}`)
+  }, [])
+
+  const Pokemon = props => (
+    <div className='pokemon-div'>
+      <h3>{`#${props.id} ${
+        props.name[0].toUpperCase() + props.name.slice(1)
+      }`}</h3>
+      <img
+        className='sprite-image'
+        src={props.sprites.front_default}
+        alt={'Pokemon Sprite'}
+      />
+      <p>
+        {`Abilities: ${
+          props.abilities[0].ability.name[0].toUpperCase() +
+          props.abilities[0].ability.name.slice(1)
+        }`}
+      </p>
+    </div>
+  )
+
+  const filteredPokemon = pokemonList.filter(pokemon =>
+    pokemon.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+  const regi = []
+  regi.push(pokemonList[375])
+  console.log(regi)
+  console.log(pokemonList)
+
+  return (
+    <div className='pokemon-container'>
+      <div>
+        <input
+          className='searchbar'
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+      </div>
+      {search !== ''
+        ? filteredPokemon
+            .map(pokemon => <Pokemon key={pokemon.id} {...pokemon} />)
+            .sort((a, b) => {
+              return a - b
+            })
+        : pokemonList
+            .map(pokemon => <Pokemon key={pokemon.id} {...pokemon} />)
+            .sort((a, b) => {
+              return a - b
+            })}
+    </div>
+  )
 }
